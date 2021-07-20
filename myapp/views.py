60b44821b4683fe4f570d173aac82852689d1a3a
basharckr <basharpmr@gmail.com>
2021-07-20 12:47:38 +0530

    hidded the creditials

--- myapp/views.py
+++ myapp/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 import requests
 from django.http import HttpResponse
 import json
+from decouple import config
 
 # Create your views here.
 
 
 params = {
     "grant_type":"password",
-    "client_id":"3MVG9fe4g9fhX0E7mOXRFi99O5F3.HzSAIP2C7Gn.c6qy7nP6yMcpCSVhwlEh_XA3NX9vn3CAYUwhnmPtR_Zf",
-    "client_secret":"7A858C616773D1975781E3BDE350AE01D1FBD3EB1CE6A0D6FE462FE48CF0BF25",
+    "client_id":config('client_id'),
+    "client_secret":config('client_secret'),
     "username":"bashar@ckr.com",
-    "password":"salesforce@123XX5BvlxisDpM68H5RS7lYqVr"
+    "password":config('password')
 }
 r = requests.post("https://login.salesforce.com/services/oauth2/token",params=params)
 access_token = r.json().get("access_token")
